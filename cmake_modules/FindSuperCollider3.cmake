99a60a3ec7842f7cfd826697a3077005561dedcb
Holger Ballweg <holger@music-nerds.net>
2012-04-08 20:17:05 +0200

    Renamed SC_PATH to SC3_SOURCES_PATH

--- cmake_modules/FindSuperCollider3.cmake
+++ cmake_modules/FindSuperCollider3.cmake
@@ -13,11 +13,15 @@
 # Find the telltale header file
 #
 GET_FILENAME_COMPONENT(SOURCEPARENT "${CMAKE_CURRENT_SOURCE_DIR}" PATH)
-find_path(SC_PATH NAMES include/plugin_interface/SC_PlugIn.h
-	PATHS "${SOURCEPARENT}"
-	PATH_SUFFIXES SuperCollider)
+find_path(
+  SC3_SOURCES_PATH NAMES include/plugin_interface/SC_PlugIn.h
+  HINTS "~/src/supercollider" "~/dev/supercollider"
+  PATHS "${SOURCEPARENT}"
+  PATH_SUFFIXES SuperCollider
+  DOC "Path to the SuperCollider source and header files.")
 
-set(SC_FOUND FALSE)
-if(IS_DIRECTORY ${SC_PATH})
-	set(SC_FOUND TRUE)
+set(SC3_SOURCES_FOUND FALSE)
+
+if(IS_DIRECTORY ${SC3_SOURCES_PATH})
+  set(SC3_SOURCES_FOUND TRUE)
 endif()
